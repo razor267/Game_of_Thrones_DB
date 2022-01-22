700d22dc03ae5f880b2e2607f1ab09af3c0e2131
Petr Baiborin <razor267@mail.ru>
2022-01-22 17:17:11 +0300

    DZ. LES 98

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
+import './app.css';
 
 
-const App = () => {
-    return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+export default class App extends Component {
+
+    state = {
+        showRandomChar: true
+    };
+
+    toggleRandomChar = () => {
+        this.setState((state) => {
+            return {
+                showRandomChar: !state.showRandomChar
+            }
+        });
+    };
+
+    render() {
+        const char = this.state.showRandomChar ? <RandomChar/> : null;
+
+        return (
+            <>
+                <Container>
+                    <Header/>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col lg={{size: 5, offset: 0}}>
+                            {char}
+                            <button
+                                className="toggle-btn"
+                                onClick={this.toggleRandomChar}>Toggle random character</button>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md='6'>
+                            <ItemList/>
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails/>
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    }
 };
-
-export default App;
--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -60,23 +60,23 @@ const View = ({char}) => {
     const {name, gender, born, died, culture} = char;
     return (
         <>
-            <h4>Random Character: {name}</h4>
+            <h4>Random Character: {name ? name : 'no data'}</h4>
             <ul className="list-group list-group-flush">
                 <li className="list-group-item d-flex justify-content-between">
                     <span className="term">Gender </span>
-                    <span>{gender}</span>
+                    <span>{gender ? gender : 'no data'}</span>
                 </li>
                 <li className="list-group-item d-flex justify-content-between">
                     <span className="term">Born </span>
-                    <span>{born}</span>
+                    <span>{born ? born : 'no data'}</span>
                 </li>
                 <li className="list-group-item d-flex justify-content-between">
                     <span className="term">Died </span>
-                    <span>{died}</span>
+                    <span>{died ? died : 'no data'}</span>
                 </li>
                 <li className="list-group-item d-flex justify-content-between">
                     <span className="term">Culture </span>
-                    <span>{culture}</span>
+                    <span>{culture ? culture : 'no data'}</span>
                 </li>
             </ul>
         </>
