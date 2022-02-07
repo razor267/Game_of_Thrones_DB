0f4d50fe97c40b339d76b02dc678c3fe154996d1
Petr Baiborin <razor267@mail.ru>
2022-02-07 21:45:53 +0300

    hooks useState, useEffect. LES 104

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,80 +1,45 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import './itemList.css';
 import Spinner from "../spinner";
-import propTypes from 'prop-types';
-import gotService from "../../services/gotService";
 
-class ItemList extends Component {
+function ItemList({getData, onItemSelected, renderItem}) {
 
-    static defaultProps = {
-        onItemSelected: () => {
-        }
-    }
+    const [itemList, updateList] = useState([]);
 
-    static propTypes = {
-        onItemSelected: propTypes.func
-    }
+    useEffect(() => {
+        getData()
+            .then((data) => {
+                updateList(data)
+            })
+    }, []);
 
-    renderItems(arr) {
+    function renderItems(arr) {
         return arr.map((item) => {
 
             const {id} = item;
-            const label = this.props.renderItem(item);
+            const label = renderItem(item);
             return (
                 <li
                     key={id}
                     className="list-group-item"
-                    onClick={() => this.props.onItemSelected(id)}>
+                    onClick={() => onItemSelected(id)}>
                     {label}
                 </li>
             )
         })
     }
 
-    render() {
-
-        const {data} = this.props;
-        const items = this.renderItems(data);
-
-        return (
-            <ul className="item-list list-group">
-                {items}
-            </ul>
-        );
+    if (!itemList) {
+        return <Spinner/>
     }
+
+    const items = renderItems(itemList);
+
+    return (
+        <ul className="item-list list-group">
+            {items}
+        </ul>
+    );
 }
 
-const withData = (View, getData) => {
-
-    return class extends Component {
-
-        state = {
-            data: null
-        }
-
-        componentDidMount() {
-
-            getData()
-                .then((data) => {
-                    this.setState({
-                        data
-                    })
-                })
-        }
-
-        render() {
-
-            const {data} = this.state;
-
-            if (!data) {
-                return <Spinner/>
-            }
-
-            return <View {...this.props} data={data}/>
-        }
-    }
-}
-
-const {getAllCharacters} = new gotService()
-
-export default withData(ItemList, getAllCharacters);
+export default ItemList;
