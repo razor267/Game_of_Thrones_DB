389335ac24368959309867c86f58a34202947521
Petr Baiborin <razor267@mail.ru>
2022-02-12 23:12:24 +0300

    JEST tests. LES 113

---
+++ src/components/randomChar/RandomChar.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import RandomChar from "./randomChar";
+import {shallow} from 'enzyme';
+
+describe('Testing <RandomChar/>', () => {
+    const char = shallow(<RandomChar/>);
+
+    describe('Testing snap & state', () => {
+
+        it('RandomChar have rendered correctly', () => {
+            expect(char).toMatchSnapshot();
+        });
+
+        it('RandomChar state "char" is empty object', () => {
+            expect(char.state().char).toBeObject();
+        });
+
+        it('RandomChar state "loading" is true', () => {
+            expect(char.state().loading).toBeTruthy();
+        });
+
+        it('RandomChar state "error" is false', () => {
+            expect(char.state().error).toBeFalsy();
+        });
+    });
+    describe('Handlers tests', () => {
+        it('Testing onChaLoaded', () => {
+            char.instance().onCharLoaded();
+            expect(char.state().loading).toBeFalsy();
+        });
+
+        it('Testing onError', () => {
+            char.instance().onError();
+            expect(char.state().loading).toBeFalsy();
+            expect(char.state().error).toBeTruthy();
+        });
+
+        it('Testing updateChar', () => {
+            char.instance().updateChar();
+            expect(char.state().loading).toBeFalsy();
+        });
+    });
+});
